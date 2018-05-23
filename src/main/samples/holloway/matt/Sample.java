c6c1f878d338e2b7aef3ec8c0078046eba3b00c9
Matt Holloway <hollowmatt@gmail.com>
2018-05-22 21:22:40 -0400

    nothing to see here, move along

--- src/main/samples/holloway/matt/Sample.java
+++ src/main/samples/holloway/matt/Sample.java
@@ -1,7 +1,10 @@
-package holloway.matt.sample;
+package holloway.matt;
 
 public class Sample {
   public static void main(String ... args) {
     System.out.println("matt");
+    for(String arg : args) {
+    	System.out.println(arg);
+    }
   }
 }
