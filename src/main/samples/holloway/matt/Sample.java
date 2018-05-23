e1e6911325bbe109bf65b80c8298cc803b5e98ff
Matt Holloway <hollowmatt@gmail.com>
2018-05-23 07:57:03 -0400

    static initializer

--- src/main/samples/holloway/matt/Sample.java
+++ src/main/samples/holloway/matt/Sample.java
@@ -1,10 +1,17 @@
 package holloway.matt;
 
 public class Sample {
+  private static String message = new String();
+
+  static {
+  	message = "This is a static initialized message";
+  }
+
   public static void main(String ... args) {
     System.out.println("matt");
     for(String arg : args) {
     	System.out.println(arg);
     }
+    System.out.println(message);
   }
 }
