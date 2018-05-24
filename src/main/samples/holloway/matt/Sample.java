27f853fd66035f4e2327e2fd2bea5bda97b09b19
Matt Holloway <hollowmatt@gmail.com>
2018-05-24 08:11:35 -0400

    test

--- src/main/samples/holloway/matt/Sample.java
+++ src/main/samples/holloway/matt/Sample.java
@@ -2,6 +2,8 @@ package holloway.matt;
 
 public class Sample extends Object {
   private static String message = new String();
+  private static Shape[] shapes = new Shape[2];
+
 
   static {
   	message = "This is a static initialized message";
@@ -17,5 +19,10 @@ public class Sample extends Object {
     	System.out.println(arg);
     }
     System.out.println(message);
+    shapes[0] = new Circle(2.0);
+    shapes[1] = new Rectangle(2.0, 3.0);
+
+    System.out.println(shapes[0].area());
+    System.out.println(shapes[1].area());
   }
 }
